a29341972daf165b841629257569638eded8c975
gache <erickfrancodelgado@hotmail.com>
2020-09-26 09:20:39 +0200

    slideShow is ok

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,29 +1,41 @@
-import { AfterViewInit, Component, Input, OnInit} from '@angular/core';
-import {Movie} from "../../interface/movies";
-import {Swiper} from "swiper";
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Movie } from "../../interface/movies";
+import { Swiper } from "swiper";
 
-@Component({
+@Component( {
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
-})
+} )
 export class SlideshowComponent implements OnInit, AfterViewInit {
 
-  @Input() movies:Movie[];
+  @Input() movies: Movie[];
+
+  public mySwiper: Swiper; // par default est undefined
+
   constructor() { }
 
-  ngOnInit(): void {
-    console.log(this.movies)
+  ngOnInit (): void {
+    console.log( this.movies )
   }
 
 
 
-  ngAfterViewInit(): void {
-    const mySwiper = new Swiper('.swiper-container', {
-
+  ngAfterViewInit (): void {
+    this.mySwiper = new Swiper( '.swiper-container', {
       loop: true,
+    } );
 
-    })
+
+  }
+
+  onSlideNext () {
+    this.mySwiper.slideNext();
+
+  }
+
+  onSlidePrev () {
+    this.mySwiper.slidePrev();
   }
 
 }
